995ee3c8eb66eb1c56867bafdd6c715a339e2d7d
ebagabee <ebagabe.2025@gmail.com>
2025-03-18 22:57:45 -0300

    setup: adiciona main js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import "./styles/global.scss";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.mount("#app");
